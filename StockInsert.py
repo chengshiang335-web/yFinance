28d6771b2f3c085ae2246259177cf32cd1877150
hsiang <chengshiang335@gmail.com>
2026-03-18 13:06:24 +0800

    yfinance爬蟲數據回寫到AzureDB

---
+++ StockInsert.py
@@ -0,0 +1,59 @@
+import datetime
+
+import yfinance as yf
+import pymssql
+
+import time
+
+#今日行情與指數
+def ticker_summary(id):
+    tick = yf.Ticker(id)
+    info = tick.fast_info
+    #print(info) 
+
+    print(f'開盤:{info.open}')
+    print(f'今日最高:{info.day_high}')
+    print(f'今日最低:{info.day_low}')
+    print(f'目前價格:{info.last_price}')
+    return info
+
+
+
+"""
+使用 pymssql 對資料庫進行連接
+"""
+server = "mis168db1.database.windows.net"
+database = "free-sql-db-3039299"
+user = "chengshiang"
+password = "Tools335~"  
+'''密碼洩漏問題'''
+
+INS_SQL = """
+    INSERT into dbo.TWStockDailyPrice(StockId, OpenPrice, HighPrice, LowPrice, ClosePrice) 
+    VALUES (
+        %s,
+        %s,
+        %s,
+        %s,
+        %s
+    )
+"""
+try:
+    connect = pymssql.connect(server, user, password, database)
+    cursor = connect.cursor()
+
+    print("資料庫連線成功")
+
+
+    info = ticker_summary("2330.TW")
+    cursor.execute(INS_SQL, ("2330", info.open, info.day_high, info.day_low, info.last_price))
+    # pymssql 預設設定 autocommit = false
+    connect.commit()
+
+    print("資料寫入完畢")
+    cursor.close()
+    connect.close()
+
+
+except Exception as e: 
+    print(f'連線失敗: 原因{e}')
